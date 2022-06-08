bb070a0cc218c1f1150cceac58059623cb9e5d1a
Lidor-sh <lidorschool3@gmail.com>
2022-06-09 01:06:01 +0300

    starting with the layout

---
+++ com/lidorsh/pomodoro/Main.java
@@ -0,0 +1,12 @@
+package com.lidorsh.pomodoro;
+
+import com.lidorsh.pomodoro.ui.GUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.repaint();
+    }
+
+}
